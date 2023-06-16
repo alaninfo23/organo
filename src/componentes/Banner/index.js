71bd01b87f06363fa024dc7151202abd36ffba8d
Alan Santos <js-alan@hotmail.com>
2023-06-16 10:56:40 -0300

    changes in the Banner.css to correct max-width

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,13 +1,16 @@
-import { Fragment } from 'react';
-import './Banner.css';
+import { Fragment } from "react";
+import "./Banner.css";
 function Banner() {
-    return (
-        <>
-        <header className='banner'>
-        <img src="/imagens/banner.png" alt='O banner principal da página do Organo'/>
-        </header>
-        </>
-    );
+  return (
+    <>
+      <header className="banner">
+        <img
+          src="/imagens/banner.png"
+          alt="O banner principal da página do Organo"
+        />
+      </header>
+    </>
+  );
 }
 
-export default Banner
+export default Banner;
